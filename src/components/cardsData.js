9cd700712d3e3b8588a4ef5cbcf53b2bcc7a83d6
omarmode <omarmode35@gmail.com>
2024-07-11 17:56:04 +0300

    a

---
+++ src/components/cardsData.js
@@ -0,0 +1,49 @@
+export const cardsData = [
+  {
+    id: 0,
+    title: "Banners",
+    components: [
+      {
+        id: 100,
+        name: "Banners",
+        images: [
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720339525/banner-1_ocdzvu.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720339530/banner-2_s4y8tg.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720339527/banner-3_saqmcd.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720339533/banner-4_kiwju3.png",
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Categories",
+    components: [
+      {
+        id: 200,
+        name: "Categories",
+        images: [
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720340536/cat-1_jplhmk.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720340537/cat-2_acxrqu.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720340530/cat-3_fvrvsw.png",
+          "https://res.cloudinary.com/dyufmizpc/image/upload/v1720340530/cat-4_hls1km.png",
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Products",
+    components: [
+      {
+        id: 300,
+        name: "Products",
+        images: [
+          "https://res.cloudinary.com/dgswngo1l/image/upload/v1720340304/aa/p11_gyrkvv.png",
+          "https://res.cloudinary.com/dgswngo1l/image/upload/v1720340474/aa/p22_puppds.png",
+        ],
+      },
+    ],
+  },
+  { id: 3, title: "Preview", components: [] },
+];
